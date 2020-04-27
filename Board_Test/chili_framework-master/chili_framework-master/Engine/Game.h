729d2fd308beffcfe57c40fe7badf34233690af3
Pierre-Paul Corriveau <doubleppcorriveau@gmail.com>
2020-04-27 17:05:57 -0400

    détachement du lien entre la partie Board et la partie des objets

--- Board_Test/chili_framework-master/chili_framework-master/Engine/Game.h
+++ Board_Test/chili_framework-master/chili_framework-master/Engine/Game.h
@@ -24,6 +24,7 @@
 #include "Mouse.h"
 #include "Board.h"
 #include "Time.h"
+#include "EmoteHuman.h"
 
 class Game
 {
--- Board_Test/chili_framework-master/chili_framework-master/Engine/Screen.h
+++ Board_Test/chili_framework-master/chili_framework-master/Engine/Screen.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "EmoteHuman.h"
+#include "Graphics.h"
 
 class Screen
 {
